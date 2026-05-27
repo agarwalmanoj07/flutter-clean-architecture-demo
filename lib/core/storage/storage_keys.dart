c4ac57ad817fe2f085283c12bf9931e00b116ea4
Manoj <agarwalmanoj07@gmail.com>
2026-05-27 19:07:16 +0530

    Implement secure token storage

--- lib/core/storage/storage_keys.dart
+++ lib/core/storage/storage_keys.dart
@@ -1,3 +1,7 @@
 class StorageKeys {
   static const String users = 'users';
+
+  static const String accessToken = 'access_token';
+
+  static const String refreshToken = 'refresh_token';
 }
--- lib/features/auth/data/datasources/auth_local_data_source.dart
+++ lib/features/auth/data/datasources/auth_local_data_source.dart
@@ -1,7 +1,63 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../../../../core/exceptions/app_exception.dart';
+import '../../../../core/storage/storage_keys.dart';
 import '../models/auth_tokens.dart';
 
 abstract class AuthLocalDataSource {
-  Future<AuthTokens?> getTokens();
   Future<void> saveTokens(AuthTokens tokens);
+  Future<AuthTokens?> getTokens();
   Future<void> clearTokens();
+}
+
+class AuthLocalDataSourceImpl implements AuthLocalDataSource {
+  final FlutterSecureStorage secureStorage;
+
+  AuthLocalDataSourceImpl({required this.secureStorage});
+
+  @override
+  Future<void> saveTokens(AuthTokens tokens) async {
+    try {
+      await secureStorage.write(
+        key: StorageKeys.accessToken,
+        value: tokens.accessToken,
+      );
+      await secureStorage.write(
+        key: StorageKeys.refreshToken,
+        value: tokens.refreshToken,
+      );
+    } catch (e) {
+      AppException('Failed to save tokens: $e');
+    }
+  }
+
+  @override
+  Future<AuthTokens?> getTokens() async {
+    try {
+      final accessToken = await secureStorage.read(
+        key: StorageKeys.accessToken,
+      );
+      final refreshToken = await secureStorage.read(
+        key: StorageKeys.refreshToken,
+      );
+
+      if (accessToken != null && refreshToken != null) {
+        return AuthTokens(accessToken: accessToken, refreshToken: refreshToken);
+      }
+      return null;
+    } catch (e) {
+      AppException('Failed to retrieve tokens: $e');
+      return null;
+    }
+  }
+
+  @override
+  Future<void> clearTokens() async {
+    try {
+      await secureStorage.delete(key: StorageKeys.accessToken);
+      await secureStorage.delete(key: StorageKeys.refreshToken);
+    } catch (e) {
+      AppException('Failed to clear tokens: $e');
+    }
+  }
 }
